5521e96044e7328bdcb8c9d626636e3f4b29ebfd
Kalash004 <kalashnikov004@gmail.com>
2024-10-17 21:20:45 +0200

    rph/hw03:done ?

--- rph/hw/hw03/player.py
+++ rph/hw/hw03/player.py
@@ -1,11 +1,18 @@
 
 class MyPlayer:
     """_summary_ Player plays moves that give the most points.
-    """    
+    """   
+    # My player is bad when the payoff matix DD has biggest numbers example:
+        # (((4,4),(10,6)),((6,10),(20,20)))
+        #    CC     CD      DC      DD
+        # I use random tactic at this point
+        
+    PLAYER = 0
+    OPPONENT = 1
+    COOPERATE = 0
+    DEFECT = 1
+    
     def __init__(self, pay_off_matrix, round_count=None): # type: ignore
-        self.PLAYER = 0
-        self.COOPERATE = 0
-        self.DEFECT = 1
         self.round_count = round_count
         self.last_round_num = 0
         self.pay_off_matrix = pay_off_matrix
@@ -13,32 +20,78 @@ class MyPlayer:
         self.play_this_round = None
         self.smart_opponent = True
         self.local_history = []
+        self.playing_start_tactic = True
             
             
     def select_move(self) -> bool:
         if self.last_round_num == 0:
-            self.play_start_tactic()
-            self.increment_round_by_one()
+            self._set_play_start_tactic()
+            self._increment_round_by_one()
             return self.play_this_round
         
-        if self.last_round_num > 2:
-            self.smart_opponent = self.are_opponents_moves_similar()
+        if self.last_round_num >= 2:
+            self._set_is_opponent_smart()
             
-        if not self.smart_opponent:
-                self.play_best_move_based_on_stupid_enemy()
+        if not self.smart_opponent and self.last_round_num >= 2:
+            self._set_playing_start_tactic(False)
+            self._set_play_best_move_based_on_stupid_enemy()
+        elif not self.playing_start_tactic:
+            self._set_playing_start_tactic(True)
+            self._set_play_start_tactic()
     
-        self.save_local_history()
-        self.increment_round_by_one()
+        self._record_local_history()
+        self._increment_round_by_one()
         return self.play_this_round 
+    
+    def _set_playing_start_tactic(self, status):
+        self.playing_start_tactic = status
     
     def record_last_moves(self, my_last_move, opponent_last_moves):
         my_l_move = 1 if my_last_move else 0
         opp_move = 1 if opponent_last_moves else 0
-        if self.local_history[self.last_round_num] != my_l_move: ## corruption check
+        if self.last_round_num != 1 and self.local_history[self.last_round_num-2] != my_l_move: ## corruption check
             self.move_history.append((None,None)) ## corrupted data 
         self.move_history.append((my_l_move, opp_move))    
 
-    def play_start_tactic(self) -> bool:
+    def _set_play_start_tactic(self) -> bool:
+        if self.pay_off_matrix[self.DEFECT][self.DEFECT][self.PLAYER] > self.pay_off_matrix[self.COOPERATE][self.COOPERATE][self.PLAYER]:
+            self._set_random_tactic()
+            return
+        self._set_counting_tactic()
+
+    # TODO: If we see that opponent always chooses defect/cooperate - find a better move
+    # TODO: If we see that opponent uses same tactic as me - find a better move
+    def _are_opponents_moves_similar(self):
+        pre_last_round = self._get_enemy_move_history(self.last_round_num - 1)
+        last_round = self._get_enemy_move_history(self.last_round_num)
+        if None in (pre_last_round, last_round):
+            return
+        return not pre_last_round == last_round
+        
+    def _set_is_opponent_smart(self):
+        self.smart_opponent = self._are_opponents_moves_similar()
+        
+    def _increment_round_by_one(self):
+        self.last_round_num += 1
+    
+    def _get_history(self, round):
+        round -= 1 # converting round format to array format
+        if self.move_history[round][0] == None or self.move_history[round][1] == None:
+            return None
+        return self.move_history[round]
+        
+    def _set_play_best_move_based_on_stupid_enemy(self):
+        enemy_move = self._get_enemy_move_history(self.last_round_num)
+        defect_points = self.pay_off_matrix[self.DEFECT][enemy_move][self.PLAYER]
+        cooperate_points = self.pay_off_matrix[self.COOPERATE][enemy_move][self.PLAYER]
+        self.play_this_round =  False if cooperate_points > defect_points else True
+    
+    def _set_random_tactic(self):
+        import random
+        choice = (True, False)
+        self.play_this_round = random.choice(choice)
+    
+    def _set_counting_tactic(self):
         defect_max = self.pay_off_matrix[self.DEFECT][self.DEFECT][self.PLAYER] + self.pay_off_matrix[self.DEFECT][self.COOPERATE][self.PLAYER]
         cooperate_max = self.pay_off_matrix[self.COOPERATE][self.COOPERATE][self.PLAYER] + self.pay_off_matrix[self.COOPERATE][self.DEFECT][self.PLAYER]
         if defect_max > cooperate_max:
@@ -47,47 +100,58 @@ class MyPlayer:
             self.play_this_round = False # Cooperate
         if cooperate_max == defect_max: # Default
             self.play_this_round = False
-        
-    # TODO: If we see that opponent always chooses defect/cooperate - find a better move
-    # TODO: If we see that opponent uses same tactic as me - find a better move
-    def are_opponents_moves_similar(self):
-        pre_last_round = self.get_history(self.last_round_num - 1)
-        last_round = self.get_history(self.last_round_num)
-        if None in (pre_last_round, last_round):
-            return
-        return self.get_history(self.last_round_num - 2) == self.get_history(self.last_round_num - 1)
-        
-    def increment_round_by_one(self):
-        self.last_round_num += 1
     
-    def get_history(self, round):
-        print(self.move_history)
-        if self.move_history[round][0] == None or self.move_history[round][1]:
-            return None
-        return self.move_history[round]
-        
-    def play_best_move_based_on_stupid_enemy(self):
-        enemy_move = self.get_history(self.last_round_num)
-        defect_points = self.pay_off_matrix[self.DEFECT][enemy_move][self.PLAYER]
-        cooperate_points = self.pay_off_matrix[self.COOPERATE][enemy_move][self.PLAYER]
-        self.play_this_round =  False if cooperate_points > defect_points else True
+    def _get_enemy_move_history(self,round):
+        return self._get_history(round)[self.OPPONENT]
     
-    def save_local_history(self):
-        self.local_history.append(self.play_this_round)
+    def _record_local_history(self):
+        my_move = 1 if self.play_this_round else 0
+        self.local_history.append(my_move)
+    
+    
+class MyStupidPlayer:
+    def __init__(self, x ,y):
+        pass
+    
+    def select_move(self):
+        return False
+    
+    def record_last_moves(self, one, two):
+        pass
+    
+    
+class MyRandomPlayer:
+    def __init__(self, x ,y):
+        pass
+    
+    def select_move(self):
+        import random
+        choice = (True, False)
+        return random.choice(choice)
+    
+    def record_last_moves(self, one, two):
+        pass
+    
     
 if __name__ == "__main__":
-        matrix =  (((4,4),(1,6)),((6,1),(2,2)))
+        matrix =  ( ((5,5),(1,70)) , ((70,1),(2,2)) )
         
         round_count = 10
         
         player1 = MyPlayer(matrix, round_count)
-        player2 = MyPlayer(matrix, round_count)
+        player2 = MyStupidPlayer(matrix, round_count)
+        
+        points_p1 = 0
+        points_p2 = 0
         
         for i in range(0, 9):
             p1_move = player1.select_move()
             p2_move = player2.select_move()
-            print(f"Player 1: {p1_move} X Player 2: {p2_move}")
+            points_p1 += matrix[p1_move][p2_move][0]
+            points_p2 += matrix[p1_move][p2_move][1]
+            print(f"Player 1: {p1_move} points: {points_p1} X Player 2: {p2_move} points: {points_p2}")
             player1.record_last_moves(p1_move, p2_move)
             player2.record_last_moves(p2_move, p1_move)
+            
         
-        print(mp.select_move())
+        # print(mp.select_move())
