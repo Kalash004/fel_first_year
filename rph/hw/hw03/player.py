9c66da973f0ad2604b6357887b1e26c9915cfc4e
Kalash004 <kalashnikov004@gmail.com>
2024-10-18 22:01:42 +0200

    rph/hw03-04: Micro fix

--- rph/hw/hw03/player.py
+++ rph/hw/hw03/player.py
@@ -33,23 +33,26 @@ class MyPlayer:
             
             
     def select_move(self) -> bool:
-        if self.last_round_num == 0:
-            self._set_play_start_tactic()
-            self._increment_round_by_one()
-            return self.play_this_round
-        
-        if self.last_round_num >= 2:
-            self._set_is_opponent_smart()
+        try:
+            if self.last_round_num == 0:
+                self._set_play_start_tactic()
+                self._increment_round_by_one()
+                return self.play_this_round
             
-        if not self.smart_opponent and self.last_round_num >= 2:
-            self._set_playing_start_tactic(False)
-            self._set_play_best_move_based_on_stupid_enemy()
-        elif not self.playing_start_tactic:
-            self._set_playing_start_tactic(True)
-            self._set_play_start_tactic()
-    
-        self._record_local_history()
-        self._increment_round_by_one()
+            if self.last_round_num >= 2:
+                self._set_is_opponent_smart()
+                
+            if not self.smart_opponent and self.last_round_num >= 2:
+                self._set_playing_start_tactic(False)
+                self._set_play_best_move_based_on_stupid_enemy()
+            elif not self.playing_start_tactic:
+                self._set_playing_start_tactic(True)
+                self._set_play_start_tactic()
+        
+            self._record_local_history()
+            self._increment_round_by_one()
+        except:
+            return False # incase something really bad happens ;-;
         return self.play_this_round 
     
     def _set_playing_start_tactic(self, status):
@@ -58,6 +61,8 @@ class MyPlayer:
     def record_last_moves(self, my_last_move, opponent_last_moves):
         my_l_move = 1 if my_last_move else 0
         opp_move = 1 if opponent_last_moves else 0
+        my_l_move = None if my_last_move is None else my_l_move # If None is recorded
+        opp_move = None if my_last_move is None else opp_move # 
         if self.last_round_num != 1 and self.local_history[self.last_round_num-2] != my_l_move: ## corruption check
             self.move_history.append((None,None)) ## corrupted data 
         self.move_history.append((my_l_move, opp_move))    
@@ -74,7 +79,7 @@ class MyPlayer:
         pre_last_round = self._get_enemy_move_history(self.last_round_num - 1)
         last_round = self._get_enemy_move_history(self.last_round_num)
         if None in (pre_last_round, last_round):
-            return
+            return True
         return not pre_last_round == last_round
         
     def _set_is_opponent_smart(self):
@@ -111,55 +116,58 @@ class MyPlayer:
             self.play_this_round = False
     
     def _get_enemy_move_history(self,round):
-        return self._get_history(round)[self.OPPONENT]
+        ret = self._get_history(round)
+        if ret is None:
+            return None
+        return ret[self.OPPONENT]
     
     def _record_local_history(self):
         my_move = 1 if self.play_this_round else 0
         self.local_history.append(my_move)
     
     
-# class MyStupidPlayer:
-#     def __init__(self, x ,y):
-#         pass
+class MyStupidPlayer:
+    def __init__(self, x ,y):
+        pass
     
-#     def select_move(self):
-#         return True
+    def select_move(self):
+        return True
     
-#     def record_last_moves(self, one, two):
-#         pass
+    def record_last_moves(self, one, two):
+        pass
     
     
-# class MyRandomPlayer:
-#     def __init__(self, x ,y):
-#         pass
+class MyRandomPlayer:
+    def __init__(self, x ,y):
+        pass
     
-#     def select_move(self):
-#         import random
-#         choice = (True, False)
-#         return random.choice(choice)
+    def select_move(self):
+        import random
+        choice = (True, False)
+        return random.choice(choice)
     
-#     def record_last_moves(self, one, two):
-#         pass
+    def record_last_moves(self, one, two):
+        pass
     
     
 if __name__ == "__main__":
-        # matrix =  ( ((5,5),(1,70)) , ((70,1),(200,200)) )
+        matrix =  ( ((5,5),(1,70)) , ((70,1),(200,200)) )
         
-        # round_count = 100
+        round_count = 100
         
-        # player1 = MyPlayer(matrix, round_count)
-        # player2 = MyRandomPlayer(matrix, round_count)
+        player1 = MyPlayer(matrix, round_count)
+        player2 = MyRandomPlayer(matrix, round_count)
         
-        # points_p1 = 0
-        # points_p2 = 0
+        points_p1 = 0
+        points_p2 = 0
         
-        # for i in range(0, round_count):
-        #     p1_move = player1.select_move()
-        #     p2_move = player2.select_move()
-        #     points_p1 += matrix[p1_move][p2_move][0]
-        #     points_p2 += matrix[p1_move][p2_move][1]
-        #     print(f"Player 1: {p1_move} points: {points_p1} X Player 2: {p2_move} points: {points_p2}")
-        #     player1.record_last_moves(p1_move, p2_move)
-        #     player2.record_last_moves(p2_move, p1_move)
+        for i in range(0, round_count):
+            p1_move = player1.select_move()
+            p2_move = player2.select_move()
+            points_p1 += matrix[p1_move][p2_move][0]
+            points_p2 += matrix[p1_move][p2_move][1]
+            print(f"Player 1: {p1_move} points: {points_p1} X Player 2: {p2_move} points: {points_p2}")
+            player1.record_last_moves(p1_move, False)
+            player2.record_last_moves(None, None)
         pass    
         
