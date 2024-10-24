471606a96b363bd34b02d7e94c3cec513d01f123
Anton Kalashnikov <kalasan1@bullseye>
2024-10-24 09:56:34 +0200

    rph/hw/reversi

---
+++ rph/hw/reversi/player.py
@@ -0,0 +1,15 @@
+class GameState:
+    def __init__(board):
+        self.board = boards
+        
+
+class MyPlayer:
+    def __init__(my_color, opponent_color):
+        self my_color = my_color
+        self opponent_color = opponent_color
+        
+    def select_move(board) -> tuple('int row','int column'): 
+        # todo
+        
+    def _find_playable_moves(board) -> list(tuple('int row', 'int column')):
+        
