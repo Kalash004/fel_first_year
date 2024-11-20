6f40ed622757d1336dbe2c3f4bab1692014787e0
Kalash004 <kalashnikov004@gmail.com>
2024-11-20 12:20:16 +0100

    cpl/lab09

--- cpl/labs/lab09/cv1.c
+++ cpl/labs/lab09/cv1.c
@@ -3,21 +3,19 @@
 #include <stdlib.h>
 
 void* myThreadFunction(void *arguments){
-    size_t id = *(size_t*)arguments;
-    printf("Thread running: %li.\n", id);
+    size_t *id = (size_t*)arguments;
+    *id += 2;
     return NULL;
 }
 
 int main() {
-    size_t thread_count = 50;
+    size_t thread_count = 10000;
     pthread_t pool[thread_count];
-    size_t ids[thread_count];
+    size_t id = 0;
     size_t i;
     for (i = 0; i < thread_count; ++i) {
         pthread_t thread;
-        // printf("Started %li\n", i);
-        ids[i] = i;
-        pthread_create(&thread, NULL, myThreadFunction, (void *)(ids+i));
+        pthread_create(&thread, NULL, myThreadFunction, (void *)&id);
         pool[i] = thread;
     }
     printf("Main Thread.\n");
@@ -25,5 +23,6 @@ int main() {
         pthread_t t = pool[i];
         pthread_join(t, NULL); 
     }
+    printf("id: %li",id);
     return 0;
 }
