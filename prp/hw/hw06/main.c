0a5296e41aaadcf3ddc8692a9a0e958cdc8eec13
Kalash004 <kalashnikov004@gmail.com>
2024-11-28 17:03:06 +0100

    prp/hw06: done with mandatory

--- prp/hw/hw06/main.c
+++ prp/hw/hw06/main.c
@@ -1,6 +1,100 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// ----------------- Error handling -------------------------------------
+#ifndef STDIOH
+#define STDIOH
+#include <stdio.h>
+#endif
+
+#ifndef STDLIBH
+#define STDLIBH
+#include <stdlib.h>
+#endif
+
+#ifndef STRINGH
+#define STRINGH
+#include <string.h>
+#endif
+
+#ifndef bool
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
+#endif
+
+typedef struct
+{
+    int code;
+    char *message;
+} SError;
+typedef enum
+{
+    UNKNOWN_ERROR_CODE,
+    BAD_INPUT_ERR_CODE,
+    END
+} EError_codes;
+SError errors[] = {
+    {.code = -1, .message = "Error: Unknown error code: %d"},
+    {.code = 100, .message = "Error: Chybny vstup!"},
+};
+void handle_fatal_error(int code);
+void handle_non_fatal_error(int code);
+void print_error_message(int code);
+void get_error_code_to_message(int code, char buffer[], unsigned int buffer_size);
+int get_error_output_code(int code);
+/// @brief Same as handle_non_fatal_error but exits the program at the end
+/// @param code Error code number
+void handle_fatal_error(int code)
+{
+    print_error_message(code);
+    int err_code = get_error_output_code(code);
+    exit(err_code);
+}
+/// @brief Finds error code, and prints it
+/// @param code Error code number
+void handle_non_fatal_error(int code)
+{
+    print_error_message(code);
+}
+/// @brief Prints error message from char *error_messages[]
+/// @param code Error code number
+void print_error_message(int code)
+{
+    char buffer[100];
+    get_error_code_to_message(code, &buffer[0], 100);
+    fprintf(stderr, "%s\n", buffer);
+}
+/// @brief Goes thru SError errors[] and finds error by code. If doesnt exist returns Unknown error code
+/// @param code int: Error code number
+/// @param buffer char[]: Buffer to place message to
+/// @param buffer_size int: Size of the buffer
+void get_error_code_to_message(int code, char buffer[], unsigned int buffer_size)
+{
+    SError error;
+    if (code > END)
+    {
+        error = errors[UNKNOWN_ERROR_CODE];
+        snprintf(buffer, buffer_size, error.message, code);
+        return;
+    }
+    error = errors[code];
+    snprintf(buffer, buffer_size, "%s", error.message);
+    return;
+}
+int get_error_output_code(int code)
+{
+    SError error;
+    if (code > END)
+    {
+        error = errors[UNKNOWN_ERROR_CODE];
+        return error.code;
+    }
+    return errors[code].code;
+}
+// ----------------- Error handling -------------------------------------
+
 typedef struct
 {
     size_t width;
@@ -24,9 +118,15 @@ void print_matrix(Matrix matrix);
 
 Matrix *handle_multiplication(Matrix matrix_a, Matrix matrix_b);
 
+void multiply_row_col(Matrix *target, size_t row_a, size_t col_b, Matrix source_a, Matrix source_b);
+
 Matrix *handle_addition(Matrix matrix_a, Matrix matrix_b);
 
 Matrix *handle_subtraction(Matrix matrix_a, Matrix matrix_b);
+
+int get_matrix_cell_value(Matrix _target, size_t height, size_t width);
+
+void set_matrix_cell_value(Matrix *_target, size_t height, size_t width, int value);
 
 Matrix *create_matrix(size_t height, size_t width);
 
@@ -40,7 +140,13 @@ int main(void)
     Matrix *matrix_c = calculate_matrices(matrix_a, matrix_b, sign);
     // print matrix
     print_matrix(*matrix_c);
+    // cleanup
+    free_matrix(matrix_a);
+    free_matrix(matrix_b);
+    free_matrix(matrix_c);
 }
+
+
 
 Matrix *get_matrix_from_input()
 {
@@ -49,9 +155,9 @@ Matrix *get_matrix_from_input()
     size_t width = 0;
     size_t height = 0;
     int read_result = get_matrix_size(&width, &height);
-    if (!read_result)
+    if (read_result)
     {
-        // TODO: Handle scanf error
+        handle_fatal_error(BAD_INPUT_ERR_CODE);
     }
     // create matrix_a array
     int *matrix_arr = malloc(sizeof(int) * (width * height));
@@ -70,7 +176,6 @@ int get_matrix_size(size_t *width, size_t *height)
     int res = scanf("%lu %lu", &temp_height, &temp_width);
     if (res != 2)
     {
-        // TODO: Handle scanf error
         return -1;
     }
     *height = temp_height;
@@ -78,7 +183,6 @@ int get_matrix_size(size_t *width, size_t *height)
     return 0;
 }
 
-// Possible exception if data is not the right size TODO: handle
 void load_data_from_input_to_array(int *matrix_array, size_t height, size_t width)
 {
     size_t matrix_arr_index = 0;
@@ -90,11 +194,10 @@ void load_data_from_input_to_array(int *matrix_array, size_t height, size_t widt
             if (w == width - 1)
             {
                 scanf("%i", &temp);       // if last in row dont scanf space
-                char garbage = getchar(); // TODO: check for this
+                char garbage = getchar();
                 if (garbage != EOF && garbage != '\n')
                 {
-                    // TODO: handle
-                    putchar(garbage);
+                    handle_fatal_error(BAD_INPUT_ERR_CODE);
                 }
             }
             else
@@ -165,8 +268,7 @@ Matrix *handle_multiplication(Matrix matrix_a, Matrix matrix_b)
 {
     if (matrix_a.width != matrix_b.height)
     {
-        // TODO: handle
-        exit(1);
+        handle_fatal_error(BAD_INPUT_ERR_CODE);
     }
     Matrix *target = create_matrix(matrix_a.height, matrix_b.width);
     for (size_t row_a = 0; row_a < matrix_a.height; ++row_a)
@@ -195,8 +297,7 @@ Matrix *handle_addition(Matrix matrix_a, Matrix matrix_b)
 {
     if (matrix_a.width != matrix_b.width || matrix_a.height != matrix_b.height)
     {
-        // TODO: handle
-        exit(1);
+        handle_fatal_error(BAD_INPUT_ERR_CODE);
     }
     Matrix *target = create_matrix(matrix_a.height, matrix_a.width);
     for (size_t row = 0; row < matrix_a.height; ++row)
@@ -216,8 +317,7 @@ Matrix *handle_subtraction(Matrix matrix_a, Matrix matrix_b) // Its not DRY (don
 {
     if (matrix_a.width != matrix_b.width || matrix_a.height != matrix_b.height)
     {
-        // TODO: handle
-        exit(1);
+        handle_fatal_error(BAD_INPUT_ERR_CODE);
     }
     Matrix *target = create_matrix(matrix_a.height, matrix_a.width);
     for (size_t row = 0; row < matrix_a.height; ++row)
