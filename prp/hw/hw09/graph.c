c9c767df9fdfb138b1646aa154fdd6335a13696b
Kalash004 <kalashnikov004@gmail.com>
2024-12-21 18:22:20 +0100

    prp/hw09: working on saving and writing txt

--- prp/hw/hw09/graph.c
+++ prp/hw/hw09/graph.c
@@ -4,6 +4,7 @@
 #ifndef STDLIBH
 #define STDLIBH
 #include <stdlib.h>
+#include <stdio.h>
 #endif
 
 #define MALLOCED_OJB_ARR_SIZE 1000
@@ -121,12 +122,23 @@ void free_objects()
 #include <stdbool.h>
 #include <stdio.h>
 
+#define START_BUF_SIZE 20
+
+int main(void)
+{
+    char *input = "test.txt";
+    graph_t *g = allocate_graph();
+    load_txt(input, g);
+    FILE *out = fopen("out.txt", "w");
+    display_graph_text(out, g);
+    free_graph(&g);
+    return 0;
+}
+
 graph_t *allocate_graph()
 {
     graph_t *target = handled_malloc(sizeof(graph_t) * 1);
-    int *points = NULL;
-    int points_count = 0;
-    edge_t **edges = NULL;
+    edge_t *edges = NULL;
     int edges_count = 0;
 
     target->edges = edges;
@@ -144,18 +156,6 @@ void free_graph(graph_t **graph)
 
 void load_txt(const char *fname, graph_t *graph)
 {
-    /**
-start end weight
-0 1 7
-0 2 9
-0 5 14
-1 2 10
-1 3 15
-2 3 11
-2 5 255
-3 4 20001
-4 5 9
-     */
 
     FILE *f = fopen(fname, "r");
     if (f == NULL)
@@ -163,30 +163,125 @@ start end weight
         printf("File not found: load_txt %s", fname);
         exit(-11);
     }
+
+    size_t used = 0;
+    size_t buf_size = START_BUF_SIZE;
+    edge_t *buffer = handled_malloc(sizeof(edge_t) * buf_size);
     while (!feof(f))
     {
-        edge_t *new_edge = handled_malloc(sizeof(edge_t) * 1);
+        if (used == buf_size - 1)
+        {
+            buf_size *= 2;
+            buffer = handled_realloc(buffer, sizeof(edge_t) * buf_size);
+        }
         int start = get_next_int_from_line(f);
         int end = get_next_int_from_line(f);
         int weight = get_next_int_from_line(f);
+        buffer[used].start_point = start;
+        buffer[used].end_point = end;
+        buffer[used].weight = weight;
+        ++used;
     }
-
+    graph->edges = buffer;
+    graph->edges_count = used + 1;
 }
 
 int get_next_int_from_line(FILE *f)
 {
-    char temp[10] = {};
+    char temp[11] = {0};
 
     char c;
     int used = 0;
     do
     {
         c = fgetc(f);
-        if (c == ' ' || c == '\n')
+        if (c == ' ' || c == '\n' || c == EOF) {
             break;
+        }
         temp[used] = c;
         ++used;
     } while (c != ' ' && c != '\n');
+    temp[used] = '\0';
     int target = atoi(temp);
     return target;
+}
+
+void display_graph_text(FILE *f, graph_t *graph)
+{
+    for (size_t i = 0; i < graph->edges_count - 1; ++i)
+    {
+        char temp[10] = {0};
+        int start = graph->edges[i].start_point;
+        intToStr(start, temp, 10);
+        print_it(f, temp);
+        fputc(' ', f);
+        fflush(stdout);
+
+        char temp2[10] = {0};
+        int end = graph->edges[i].end_point;
+        intToStr(end, temp2, 10);
+        print_it(f, temp2);
+        fputc(' ', f);
+        fflush(stdout);
+
+
+        char temp3[10] = {0};
+        int weight = graph->edges[i].weight;
+        intToStr(weight, temp3, 10);
+        print_it(f, temp3);
+        fputc('\n', f);
+        fflush(stdout);
+
+    }
+}
+
+void intToStr(int num, char *str, int buf_size)
+{
+    int i = 0, isNegative = 0;
+
+    // Handle negative numbers
+    if (num < 0)
+    {
+        isNegative = 1;
+        num = -num;
+    }
+
+    // Calculate the number of digits in the number
+    int temp = num;
+    int digits = 0;
+    do
+    {
+        ++digits;
+        temp /= 10;
+    } while (temp != 0);
+
+    if (digits > buf_size - 1)
+    {
+        // TODO: handle buffer overflow
+    }
+
+    // If negative, leave space for the '-' sign
+    if (isNegative)
+    {
+        str[0] = '-';
+        i++;
+    }
+
+    // Build the string in the correct order
+    for (int j = digits - 1; j >= 0; --j)
+    {
+        str[i + j] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    // Null-terminate the string
+    str[i + digits] = '\0';
+}
+
+void print_it(FILE *f, char *str)
+{
+    for (size_t i = 0; str[i] != '\0'; ++i)
+    {
+        fputc(str[i], f);
+    }
 }
--- prp/hw/hw09/graph.h
+++ prp/hw/hw09/graph.h
@@ -1,6 +1,7 @@
 #ifndef __GRAPH_H__
 #define __GRAPH_H__
 
+#include <stdio.h>
 typedef struct
 {
     int start_point;
@@ -10,7 +11,7 @@ typedef struct
 
 typedef struct
 {
-    edge_t **edges;
+    edge_t *edges;
     int edges_count;
 } graph_t;
 
@@ -21,6 +22,10 @@ void free_graph(graph_t **graph);
 
 /* Load a graph from the text file. */
 void load_txt(const char *fname, graph_t *graph);
+int get_next_int_from_line(FILE *f);
+void display_graph_text(FILE *f, graph_t *graph);
+void intToStr(int num, char *str, int buf_size);
+void print_it(FILE *f, char *str);
 /* Load a graph from the binary file. */
 void load_bin(const char *fname, graph_t *graph);
 
