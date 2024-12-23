08ba4454e3eee5dad8dc136abab68824fd50b5ea
Kalash004 <kalashnikov004@gmail.com>
2024-12-23 23:42:27 +0100

    cpl/final: minor

--- cpl/hw/last_hw/main.c
+++ cpl/hw/last_hw/main.c
@@ -123,11 +123,13 @@ typedef struct
 typedef struct
 {
     int age; // suppose int is enough
-    char *date;
+    int day;
+    int month;
 } options_t;
 
 typedef struct
 {
+    long long int sum_salary;
     int avg_salary;
     int jan_count;
     int feb_count;
@@ -156,9 +158,23 @@ int get_digit_count(int num);
 
 int get_age_from_DOB(char *DOB, int year);
 
+options_t parse_args(int argc, char **argv);
+
 void DOB_to_int(char *DOB, int *day, int *month, int *year_c);
 
 bool compare_month_day(char *x, char *y);
+
+data_entry_t find_needed_entry_count_stats(data_entry_t *data_list, options_t opt, stats_t *save_stats_to);
+
+data_entry_t get_empty_data_entry();
+
+void print_data(data_entry_t target);
+
+void add_month_count(data_entry_t data, stats_t *save_stats_to);
+
+void print_histogram(stats_t stats);
+
+int find_most_digit_number(stats_t stats);
 
 // read in files
 
@@ -166,8 +182,20 @@ int main(int argc, char **argv)
 {
     options_t opt = parse_args(argc, argv);
     data_entry_t *data_list = get_data_data_list();
+    stats_t stats = {0};
+    data_entry_t data = find_needed_entry_count_stats(data_list, opt, &stats);
+    printf("#Average salary: %i\n", stats.avg_salary);
+    print_histogram(stats);
+    if (data.birth_day == 0 && data.birth_month == 0 && data.birth_year == 0)
+    {
+        printf("Not found");
+    }
+    else
+    {
+        print_data(data);
+    }
     free_objects();
-    return 1;
+    return 0;
 }
 
 data_entry_t *get_data_data_list()
@@ -270,7 +298,7 @@ int get_age_from_DOB(char *DOB, int year)
 
 options_t parse_args(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
         // TODO: Handle argcount;
         printf("Bad amount of args");
@@ -278,7 +306,19 @@ options_t parse_args(int argc, char **argv)
     }
     options_t target;
     target.age = atoi(argv[1]);
-    target.date = argv[2];
+
+    char day_c[3], month_c[3];
+
+    day_c[0] = (argv[2])[0];
+    day_c[1] = (argv[2])[1];
+    day_c[2] = '\0';
+
+    month_c[0] = argv[2][3];
+    month_c[1] = argv[2][4];
+    month_c[2] = '\0';
+
+    target.day = atoi(day_c);
+    target.month = atoi(month_c);
     return target;
 }
 
@@ -336,7 +376,127 @@ bool compare_month_day(char *x, char *y)
     return (month1_int == month2_int && day1_int == day2_int) ? true : false;
 }
 
-data_entry_t find_needed_entry_count_stats(data_entry_t *)
+data_entry_t find_needed_entry_count_stats(data_entry_t *data_list, options_t opt, stats_t *save_stats_to)
 {
-    int avarage_salary = 0;
+    data_entry_t target = get_empty_data_entry();
+    data_entry_t data;
+    size_t i;
+    for (i = 0; i < DATABASE_SIZE; ++i)
+    {
+        data = data_list[i];
+        save_stats_to->sum_salary += data.salary;
+        add_month_count(data, save_stats_to);
+        if (data.age_in_twenty_twentyfour != opt.age)
+            continue;
+        if (data.birth_month != opt.month || data.birth_day != opt.day)
+            continue;
+        target = data;
+        break;
+    }
+    save_stats_to->avg_salary = save_stats_to->sum_salary / (i + 1);
+    return target;
+}
+
+data_entry_t get_empty_data_entry()
+{
+    data_entry_t data;
+    data.address = "";
+    data.age_in_twenty_twentyfour = 0;
+    data.birth_day = 0;
+    data.birth_month = 0;
+    data.birth_year = 0;
+    data.occupation = "";
+    data.phone = "";
+    data.salary = -1;
+    data.name = "";
+    return data;
+}
+
+void print_data(data_entry_t target)
+{
+    char month[3], day[3];
+    sprintf(month, "%0*i", 3 - get_digit_count(target.birth_month), target.birth_month);
+    sprintf(day, "%0*i", 3 - get_digit_count(target.birth_day), target.birth_day);
+    printf("Name: %s\nAddress: %s\nPhone: %s\nDate of birth: %i-%s-%s\nOccupation: %s\nSalary: %i\n", target.name, target.address, target.phone, target.birth_year, month, day, target.occupation, target.salary);
+}
+
+void add_month_count(data_entry_t data, stats_t *save_stats_to)
+{
+    switch (data.birth_month)
+    {
+    case 1:
+        save_stats_to->jan_count += 1;
+        break;
+    case 2:
+        save_stats_to->feb_count += 1;
+        break;
+    case 3:
+        save_stats_to->mar_count += 1;
+        break;
+    case 4:
+        save_stats_to->apr_count += 1;
+        break;
+    case 5:
+        save_stats_to->may_count += 1;
+        break;
+    case 6:
+        save_stats_to->jun_count += 1;
+        break;
+    case 7:
+        save_stats_to->jul_count += 1;
+        break;
+    case 8:
+        save_stats_to->aug_count += 1;
+        break;
+    case 9:
+        save_stats_to->sep_count += 1;
+        break;
+    case 10:
+        save_stats_to->oct_count += 1;
+        break;
+    case 11:
+        save_stats_to->nov_count += 1;
+        break;
+    case 12:
+        save_stats_to->dec_count += 1;
+        break;
+    default:
+        // TODO: Handle something bad
+        break;
+    }
+}
+
+void print_histogram(stats_t stats)
+{
+    printf("#Month histogram:\n");
+    int biggest = find_most_digit_number(stats);
+    char *months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    for (size_t i = 0; i < 12; ++i)
+    {
+        printf("")
+    }
+}
+
+int find_most_digit_number(stats_t stats)
+{
+    typedef union
+    {
+        stats_t stats_u;
+        int arr[sizeof(stats_t) / sizeof(int)];
+
+    } access_stats;
+
+    access_stats access;
+    access.stats_u = stats;
+
+    int most = 0;
+    // accessing month counts with union
+    for (size_t i = 4; i < sizeof(stats_t) / sizeof(int) - 1; ++i)
+    {
+        int num = access.arr[i];
+        int dig_count = get_digit_count(num);
+        if (dig_count > most)
+            most = dig_count;
+    }
+    return most;
 }
