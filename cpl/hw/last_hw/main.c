d46341d933d17492e742f39306fc06d5b2356e8b
Kalash004 <kalashnikov004@gmail.com>
2024-12-23 19:24:47 +0100

    cpl/final: ahh

--- cpl/hw/last_hw/main.c
+++ cpl/hw/last_hw/main.c
@@ -99,6 +99,7 @@ void free_objects()
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define DATABASE_SIZE 100
 #define DATABASE_DIR "./database/"
@@ -111,7 +112,9 @@ typedef struct
     char *name;
     char *address;
     char *phone;
-    char *DOB; // date of birth
+    int birth_year;
+    int birth_month;
+    int birth_day;
     char *occupation;
     int age_in_twenty_twentyfour;
     int salary;
@@ -123,7 +126,25 @@ typedef struct
     char *date;
 } options_t;
 
-data_entry_t *read_data();
+typedef struct
+{
+    int avg_salary;
+    int jan_count;
+    int feb_count;
+    int mar_count;
+    int apr_count;
+    int may_count;
+    int jun_count;
+    int jul_count;
+    int aug_count;
+    int sep_count;
+    int oct_count;
+    int nov_count;
+    int dec_count;
+} stats_t;
+
+data_entry_t *
+get_data_data_list();
 
 data_entry_t get_data_from_file(FILE *target);
 
@@ -135,18 +156,21 @@ int get_digit_count(int num);
 
 int get_age_from_DOB(char *DOB, int year);
 
+void DOB_to_int(char *DOB, int *day, int *month, int *year_c);
+
+bool compare_month_day(char *x, char *y);
+
 // read in files
 
 int main(int argc, char **argv)
 {
-
-    data_entry_t *data_list = read_data();
-    printf("%s age: %i", data_list[0].name);
+    options_t opt = parse_args(argc, argv);
+    data_entry_t *data_list = get_data_data_list();
     free_objects();
     return 1;
 }
 
-data_entry_t *read_data()
+data_entry_t *get_data_data_list()
 {
     int buffer_size = strlen(DATABASE_DIR) + get_digit_count(DATABASE_SIZE) + 4 + 1; // null terminator 1
     char f_name[buffer_size];
@@ -180,13 +204,13 @@ data_entry_t get_data_from_file(FILE *target)
     char *occupation = remove_metadata("Occupation: ", read_line(target));
     char *salary_non_clean = remove_metadata("Salary: ", read_line(target));
     int salary = atoi(salary_non_clean);
+    DOB_to_int(DOB, &tmp.birth_day, &tmp.birth_month, &tmp.birth_year);
     tmp.name = name;
     tmp.address = address;
     tmp.phone = phone;
-    tmp.DOB = DOB;
     tmp.occupation = occupation;
     tmp.salary = salary;
-    tmp.age_in_twenty_twentyfour = get_age_from_DOB(DOB, CURRENT_YEAR);
+    tmp.age_in_twenty_twentyfour = CURRENT_YEAR - tmp.birth_year;
     return tmp;
 }
 
@@ -242,4 +266,77 @@ int get_age_from_DOB(char *DOB, int year)
     c[4] = '\0';
     int year_from_data = atoi(c);
     return year - year_from_data;
+}
+
+options_t parse_args(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        // TODO: Handle argcount;
+        printf("Bad amount of args");
+        exit(1);
+    }
+    options_t target;
+    target.age = atoi(argv[1]);
+    target.date = argv[2];
+    return target;
+}
+
+void DOB_to_int(char *DOB, int *day, int *month, int *year)
+{
+    char day_c[3], month_c[3], year_c[5];
+
+    day_c[0] = DOB[8];
+    day_c[1] = DOB[9];
+    day_c[2] = '\0';
+
+    month_c[0] = DOB[5];
+    month_c[1] = DOB[6];
+    month_c[2] = '\0';
+
+    year_c[0] = DOB[0];
+    year_c[1] = DOB[1];
+    year_c[2] = DOB[2];
+    year_c[3] = DOB[3];
+    year_c[4] = '\0';
+
+    *day = atoi(day_c);
+    *month = atoi(month_c);
+    *year = atoi(year_c);
+}
+
+bool compare_month_day(char *x, char *y)
+{
+    // Temporary arrays to store month and day substrings
+    char month1[3], day1[3];
+    char month2[3], day2[3];
+
+    // Extracting substrings for month and day from input strings
+    month1[0] = x[5];
+    month1[1] = x[6];
+    month1[2] = '\0';
+
+    day1[0] = x[8];
+    day1[1] = x[9];
+    day1[2] = '\0';
+
+    month2[0] = y[5];
+    month2[1] = y[6];
+    month2[2] = '\0';
+
+    day2[0] = y[8];
+    day2[1] = y[9];
+    day2[2] = '\0';
+
+    // Converting extracted substrings to integers
+    int month1_int = atoi(month1);
+    int day1_int = atoi(day1);
+    int month2_int = atoi(month2);
+    int day2_int = atoi(day2);
+    return (month1_int == month2_int && day1_int == day2_int) ? true : false;
+}
+
+data_entry_t find_needed_entry_count_stats(data_entry_t *)
+{
+    int avarage_salary = 0;
 }
