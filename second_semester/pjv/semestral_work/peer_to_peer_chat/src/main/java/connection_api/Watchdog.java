aa93bca5a2422ac5aee41b658bb349de61e5e2ce
Anton Kalashnikov <kalasan1@student.cvut.cz>
2025-03-27 22:39:48 +0100

    kill me

--- second_semester/pjv/semestral_work/peer_to_peer_chat/src/main/java/connection_api/Watchdog.java
+++
@@ -1,9 +0,0 @@
-package connection_api;
-
-/**
- * Watches over processes
- * if stuck terminates
- * maybe counts time ?
- */
-public class Watchdog {
-}
--- second_semester/pjv/semestral_work/peer_to_peer_chat/src/main/java/models/ui/configs/UiConfig.java
+++
@@ -1,9 +0,0 @@
-package models.ui.configs;
-
-public class UiConfig {
-    private static String title = "Chat";
-
-    public static String getTitle() {
-        return title;
-    }
-}
